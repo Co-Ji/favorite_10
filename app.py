2f3f1d6a0c24f206a019bea10a4ce3438f97a068
Lee MiHwa <bbs2824@naver.com>
2022-03-08 18:49:18 +0900

    이미화 파트 app 내용추가

--- app.py
+++ app.py
@@ -32,6 +32,52 @@ def item_get():
 #
 #     return jsonify({'msg': '응원댓글을 남겼습니다!'})
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/life_item')
+def life_item():
+    return render_template('life_item.html')
+
+@app.route('/home_elec')
+def home_elec():
+    return render_template('home_elec.html')
+
+@app.route('/sport_item')
+def sport_item():
+    return render_template('sport_item.html')
+
+
+@app.route("/item", methods=["POST"])
+def item_post():
+    count = len(num) + 1
+    item_receive = request.form['item_give']
+    url_receive = request.form['url_give']
+    recommend_receive = request.form['recommend_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    image = soup.select_one('meta[property="og:image"]')['content']
+    title = soup.select_one('meta[property="og:title"]')['content']
+
+
+    doc = {
+            'num': count,
+            'item':item_receive,
+            'image':image,
+            'title':title,
+            'url':url_receive,
+            'recommend':recommend_receive
+    }
+    db.items.insert_one(doc)
+    return jsonify({'msg':'등록 완료!'})
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
